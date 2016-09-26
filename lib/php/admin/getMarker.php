770dbe2f87b605fba24edf2eba698b0baa903033
gsalin <gerald.salin@toulouse.inra.fr>
2016-09-26 21:24:12 +0200

    correction bug #7 - titres incohérents pour les fiches visualisées sur la carte depuis l'interface d'administration

--- lib/php/admin/getMarker.php
+++ lib/php/admin/getMarker.php
@@ -29,7 +29,7 @@
 				$i = 0;
 				while ($row = mysql_fetch_array($result)) {
 				$arr[$i]['id'] = $row['id_poi'];
-				$arr[$i]['lib'] = stripslashes($row['lib_poi']);
+				$arr[$i]['lib'] = stripslashes($row['lib_subcategory']);
 				$arr[$i]['date'] = $row['datecreation_poi'];
 				$arr[$i]['desc'] = stripslashes($row['desc_poi']);
 				$arr[$i]['repgt'] = stripslashes($row['reponsegrandtoulouse_poi']);
