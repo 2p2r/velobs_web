fc5af37c7203421faf83b91fb49e5cb1bbb15899
Aziz Moussa <abdoul-aziz.moussa@toulouse.inra.fr>
2017-01-11 22:15:05 +0100

    ajout de constantes pour l'envoi des mails

--- lib/php/key.php
+++ lib/php/key.php
@@ -1,12 +1,16 @@
 <?php
 	define('HOST', 'localhost');
-	define('PORT', '*****');
+	define('PORT', '3306');
 
 	define('SGBD', 'mysql');
 
-	define('DB_USER', '********');
-	define('DB_PASS', '********');
-	define('DB_NAME', 'velobs');
+	define('DB_USER', 'xxx');
+	define('DB_PASS', 'xxx');
+	define('DB_NAME', 'xxx');
 
-	define('URL', 'app_url');
+	define('URL', 'xxx');
+	define('MAIL_ALIAS_OBSERVATION_ADHERENTS', 'xxx');
+	define('MAIL_FROM', 'xxx');
+	define('MAIL_REPLY_TO', 'xxx');
+	define('MAIL_SUBJECT_PREFIX', '[Test][VelObs]');
 ?>
