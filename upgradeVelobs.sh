5aef42821dc5cd6cd3457ed2814cd39e1f55ed75
gsalin <gerald.salin@toulouse.inra.fr>
2017-10-12 06:28:05 +0200

    #44 ajout exécution script php pour mise à jour éventuelle de la base de données

--- upgradeVelobs.sh
+++ upgradeVelobs.sh
@@ -5,12 +5,22 @@
 #renomme le dossier de production en le suffixant avec un timestamp
 #renomme le dossier des sources à passer en production avec le nom du répertoire de production
 
+defaultVersion=V1_3
+
 echo "$(tput setaf 1)Ce script doit se trouver au même niveau que les répertoires source de velobs, de production comme la version à mettre en production. Si ce n'est pas le cas, veuillez quitter et déplacer ce script et le répertoire contenant la nouvelle version de velobs au même niveau que le répertoire de velobs en production SVP.$(tput sgr 0)"
 echo ""
 echo -n "$(tput setaf 2)Nom du répertoire contenant les sources de velobs à passer en production? $(tput sgr 0)"
 read velobsDev
 echo -n "$(tput setaf 2)Nom du répertoire contenant les sources de velobs actuellement en production? $(tput sgr 0)"
 read velobsProd
+echo -n "$(tput setaf 2)Version de VelObs qui sera passée en production (important car permet d'exécuter un script correspondant, s'il existe) - défaut $defaultVersion? $(tput sgr 0)"
+read velobsVersion
+
+if [  -z $velobsVersion ]
+then
+        echo "$(tput setaf 1)La version n'a pas été spécifiée, on utilise la valeur par défaut $defalutVersion$(tput sgr 0)"
+        velobsVersion=$defaultVersion
+fi
 
 timestamp=$(date +%s)
 if [  -z $velobsDev ]
@@ -62,5 +72,15 @@ chmod 775 $velobsDev/resources/csv $velobsDev/resources/pictures
 
 echo "switch des version de velobs pour le passage en production : mv $velobsProd $velobsProd$timestamp; mv $velobsDev $velobsProd"
 mv $velobsProd $velobsProd.$timestamp;mv $velobsDev $velobsProd
+
+
+PHP=`which php`
+if [ -e $velobsProd/resources/upgrade/upgradeSQL-$velobsVersion.php ]
+then
+        echo "Exécution de la commande $PHP $velobsProd/resources/upgrade/upgradeSQL-$velobsVersion.php"
+        #$PHP $velobsProd/resources/upgrade/upgradeSQL-$velobsVersion.php
+else
+        echo "$(tput setaf 1)Le fichier $velobsProd/resources/upgrade/upgradeSQL-$velobsVersion.php n'existe pas. Il se peut qu'aucun script SQL n'existe pour ce changement de version, mais vérifiez que vous avez spécifié fidèlement la version de velobs à laquelle vous passez (de la forme Vx_y). Si vous vous êtes trompé(e), vous devrez exécuter le script en ligne de commande.$(tput sgr 0)"
+fi
 
 echo "$(tput setaf 5)La nouvelle version de velobs est installée. Merci de la tester.$(tput sgr 0)"
