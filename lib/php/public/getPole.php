daf175d219546331eba5503f7b836b2e27f5cdee
gerald2545 <gerald.salin@gmail.com>
2017-10-17 23:07:50 +0200

    affichage des contours géographiques des communes et des poles

--- lib/php/public/getPole.php
+++ lib/php/public/getPole.php
@@ -6,12 +6,13 @@
 			$link = mysql_connect(HOST,DB_USER,DB_PASS);
 			mysql_select_db(DB_NAME);	
 			
-			$sql = "SELECT id_pole, lib_pole FROM pole ORDER BY lib_pole ASC";
+			$sql = "SELECT id_pole, lib_pole, AsText(geom_pole) AS geom  FROM pole ORDER BY lib_pole ASC";
 			$result = mysql_query($sql);
 			$i = 0;
 			while ($row = mysql_fetch_array($result)){
 				$arr[$i]['id_pole'] = $row['id_pole'];
 				$arr[$i]['lib_pole'] = stripslashes($row['lib_pole']);
+				$arr[$i]['geom'] = stripslashes($row['geom']);
 				$i++;
 			}
 			echo '({"pole":'.json_encode($arr).'})';
