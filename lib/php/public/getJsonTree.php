8dd15dcd4c2886bd92bce71ef71138ab74ea65b1
gerald2545 <gerald.salin@gmail.com>
2018-01-27 08:05:06 +0100

    #96 permettre de configurer velobs pour afficher ou non par defaut les observations à la première ouverture de l'interface publique (en tous cas tant qu'il n'y a pas de cooke actif qui stocke la sélection des catégories de l'utilisateur)

--- lib/php/public/getJsonTree.php
+++ lib/php/public/getJsonTree.php
@@ -24,6 +24,7 @@
 				$json .= "text: '".addslashes($row['lib_category'])."',";
 				$json .= "iconCls: '".$row['icon_category']."',";
 				$json .= "expanded: true,";
+				$json .= "checked: true,";
 				
 				$sql2 = "SELECT subcategory.* FROM subcategory WHERE display_subcategory = TRUE AND category_id_category = ".$row['id_category']." ORDER BY treerank_subcategory ASC";
 				$result2 = mysql_query($sql2);
@@ -39,7 +40,7 @@
 							$json .= "text: '".addslashes($row2['lib_subcategory'])."',";
 							$json .= "iconCls: '".$row2['icon_subcategory']."',";
 							$json .= "leaf: true,";
-							$json .= "checked: false";
+							
 							$json .= "},";
 						}
 					}
