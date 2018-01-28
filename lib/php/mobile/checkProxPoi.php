c38bc91db5c8d2dc1c245b083a856fd20eb558cb
gerald2545 <gerald.salin@gmail.com>
2018-01-28 23:51:29 +0100

    #86 : 1 seule requête au lieu de 3 pour recupérer l'observatrion, sa commune et sa catégorie

--- lib/php/mobile/checkProxPoi.php
+++ lib/php/mobile/checkProxPoi.php
@@ -23,8 +23,34 @@
                 }
                 $centerlat = $_POST['lat'];
                 $centerlng = $_POST['lng'];
-                $sql = "SELECT  datecreation_poi, prop_poi, x(poi.geom_poi) AS X, y(poi.geom_poi) AS Y,commune_id_commune, desc_poi, num_poi, photo_poi, id_poi, lib_poi, rue_poi, status_id_status, subcategory_id_subcategory, 6371 * 2 * asin(sqrt(power(sin((".$centerlat." - abs(y(geom_poi))) * pi() / 180 / 2), 2) + cos(".$centerlat." * pi() / 180) * cos(abs(y(geom_poi)) * pi() / 180) * power(sin((".$centerlng." - x(geom_poi)) * pi() / 180 / 2), 2) )) AS distance, priorite_id_priorite FROM poi WHERE priorite_id_priorite <> 6 AND moderation_poi = 1 AND (status_id_status LIKE 1 OR status_id_status LIKE 3 OR status_id_status LIKE 5) HAVING distance < ".$buffer." ORDER BY distance";
-                //$sql = "SELECT id_poi, lib_poi, rue_poi, status_id_status, subcategory_id_subcategory, 6371 * 2 * asin(sqrt(power(sin((".$centerlat." - abs(y(geom_poi))) * pi() / 180 / 2), 2) + cos(".$centerlat." * pi() / 180) * cos(abs(y(geom_poi)) * pi() / 180) * power(sin((".$centerlng." - x(geom_poi)) * pi() / 180 / 2), 2) )) AS distance FROM poi HAVING distance < ".$buffer." ORDER BY distance";
+                $sql = "SELECT  lib_subcategory, 
+                		lib_commune, 
+                		datecreation_poi, 
+                		prop_poi, 
+                		x(poi.geom_poi) AS X, 
+                		y(poi.geom_poi) AS Y,
+                		commune_id_commune, 
+                		desc_poi, 
+                		num_poi, 
+                		photo_poi, 
+                		id_poi, 
+                		lib_poi, 
+                		rue_poi, 
+                		status_id_status, 
+                		subcategory_id_subcategory, 
+                		6371 * 2 * asin(sqrt(power(sin((".$centerlat." - abs(y(geom_poi))) * pi() / 180 / 2), 2) + cos(".$centerlat." * pi() / 180) * cos(abs(y(geom_poi)) * pi() / 180) * power(sin((".$centerlng." - x(geom_poi)) * pi() / 180 / 2), 2) )) AS distance, 
+                		priorite_id_priorite 
+                	FROM poi
+                	INNER JOIN subcategory ON subcategory.id_subcategory = poi.subcategory_id_subcategory
+                	INNER JOIN commune ON commune.id_commune = poi.commune_id_commune
+                	WHERE priorite_id_priorite <> 6 
+                				AND moderation_poi = 1 
+                				AND (status_id_status LIKE 1 OR status_id_status LIKE 3 OR status_id_status LIKE 5) 
+                	HAVING distance < ".$buffer." 
+                	ORDER BY distance";
+                if (DEBUG){
+                	error_log(date("Y-m-d H:i:s") . " " .__FUNCTION__ . " - checkProxPOI.php pour $centerlat et $centerlng \n", 3, LOG_FILE);
+                }
                 $result = mysql_query($sql);
                 $num_rows = mysql_num_rows($result);
                 if ($num_rows == 0) {
@@ -35,24 +61,21 @@
                     print '<coderetour result="'.$num_rows.'" />';
                     print '<listpoi>';
                     while ($row = mysql_fetch_array($result)) {
-                        $sql2 = "SELECT lib_subcategory FROM subcategory WHERE id_subcategory = ".$row['subcategory_id_subcategory'];
-                        $result2 = mysql_query($sql2);
-                        while ($row2 = mysql_fetch_array($result2)) {
-                            $subcategory = $row2['lib_subcategory'];
-                        }
-                        mysql_free_result($result2);
-                        $sql3 = "SELECT id_commune, lib_commune FROM commune WHERE commune.id_commune = ".$row['commune_id_commune'];
-                        $result3 = mysql_query($sql3);
-                        while ($row3 = mysql_fetch_array($result3)) {
-                            $lib_commune = $row3['lib_commune'];
-                        }
-                        mysql_free_result($result3);
+//                         $sql2 = "SELECT lib_subcategory FROM subcategory WHERE id_subcategory = ".$row['subcategory_id_subcategory'];
+//                         $result2 = mysql_query($sql2);
+//                         while ($row2 = mysql_fetch_array($result2)) {
+//                             $subcategory = $row2['lib_subcategory'];
+//                         }
+//                         mysql_free_result($result2);
+//                         $sql3 = "SELECT id_commune, lib_commune FROM commune WHERE commune.id_commune = ".$row['commune_id_commune'];
+//                         $result3 = mysql_query($sql3);
+//                         while ($row3 = mysql_fetch_array($result3)) {
+//                             $lib_commune = $row3['lib_commune'];
+//                         }
+//                         mysql_free_result($result3);
                         $distance = intval($row['distance'] * 1000);
-                        //print '<poi id="'.$row['id_poi'].'" category="'.addslashes(stripslashes($subcategory)).'" adresse="'.addslashes(stripslashes($row['rue_poi'])).'" distance="'.$distance.'" />';
-                        //print '<poi id="'.$row['id_poi'].'" category="'.addslashes(stripslashes($subcategory)).'" adresse="'.addslashes(stripslashes($row['rue_poi'])).'" distance="'.$distance.'" status="'.$row['status_id_status'].'"/>';
-                        //print '<poi id="'.$row['id_poi'].'" category="'.stripslashes($subcategory).'" adresse="'.stripslashes($row['rue_poi']).'" distance="'.$distance.'" status="'.$row['status_id_status'].'" photo="'.$row['photo_poi'].'" num="'.stripslashes($row['num_poi']).'" desc="'.stripslashes($row['desc_poi']).'" ville="'.stripslashes($lib_commune).'" />';
                         print '<poi id="'.$row['id_poi'].'">';
-                            print '<category><![CDATA['.stripslashes($subcategory).']]></category>';
+                            print '<category><![CDATA['.stripslashes($row['lib_subcategory']).']]></category>';
                             print '<adresse><![CDATA['.stripslashes($row['rue_poi']).']]></adresse>';
                             print '<desc><![CDATA['.stripslashes($row['desc_poi']).']]></desc>';
                             print '<distance>'.$distance.'</distance>';
@@ -61,7 +84,7 @@
                             print '<num><![CDATA['.stripslashes($row['num_poi']).']]></num>';
 			    			print '<latitude>'.$row['Y'].'</latitude>';
                             print '<longitude>'.$row['X'].'</longitude>';
-                            print '<ville><![CDATA['.stripslashes($lib_commune).']]></ville>';
+                            print '<ville><![CDATA['.stripslashes($row['lib_commune']).']]></ville>';
 			    			print '<prop><![CDATA['.stripslashes($row['prop_poi']).']]></prop>';
 			    			print '<dateCreation><![CDATA['.stripslashes($row['datecreation_poi']).']]></dateCreation>';
                         print '</poi>';
