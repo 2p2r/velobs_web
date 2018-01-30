6cb17e67d17d7b14ba27a3837ecfe58ac680b082
gerald2545 <gerald.salin@gmail.com>
2018-01-30 07:22:16 +0100

    #98 correction requête + affichage nombre d'observation correspondant

--- lib/php/public/getJsonTree.php
+++ lib/php/public/getJsonTree.php
@@ -17,11 +17,13 @@
 			while ($row = mysql_fetch_array($result)){
 				$sql2 = "SELECT distinct(subcategory.id_subcategory), 
 							subcategory.lib_subcategory, 
-							subcategory.icon_subcategory 
+							subcategory.icon_subcategory,
+							COUNT(poi.id_poi) as nb_poi 
 						FROM subcategory 
 						INNER JOIN poi ON (poi.subcategory_id_subcategory = subcategory.id_subcategory) 
 						WHERE display_subcategory = TRUE AND 
 							category_id_category =  ".$row['id_category']."
+						GROUP BY subcategory.id_subcategory 
 						ORDER BY treerank_subcategory ASC";
 				$result2 = mysql_query($sql2);
 				if (mysql_num_rows($result2) > 0){
@@ -36,7 +38,7 @@
 					while ($row2 = mysql_fetch_array($result2)){
 							$json .= "{";
 							$json .= "id: '".$row2['id_subcategory']."',";
-							$json .= "text: '".addslashes($row2['lib_subcategory'])."',";
+							$json .= "text: '".addslashes($row2['lib_subcategory'])." (".$row2['nb_poi'].")',";
 							$json .= "iconCls: '".$row2['icon_subcategory']."',";
 							$json .= "leaf: true,";
 							$json .= "},";
@@ -57,7 +59,6 @@
 
 			mysql_free_result($result);
 			mysql_free_result($result2);
-			mysql_free_result($result3);
 			mysql_close($link);
 			break;
 		case 'postgresql':
