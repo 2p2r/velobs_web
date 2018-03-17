08cfa2ab4f435c08e67dc8ead598fb648e93f64d
gerald2545 <gerald.salin@gmail.com>
2018-03-17 07:58:47 +0100

    #105 : hashing passwords with blowfish, correct unnecessary escape string

--- lib/php/admin/adminfunction.php
+++ lib/php/admin/adminfunction.php
@@ -1889,7 +1889,7 @@ Lien vers la modération : ".URL.'/admin.php?id='.$arrayObs['id_poi']."\n".
 
 				$id_users = mysql_real_escape_string($_POST['id_users']);
 				$lib_users = mysql_real_escape_string($_POST['lib_users']);
-				$pass_users = mysql_real_escape_string($_POST['pass_users']);
+				$pass_users = create_password_hash($_POST['pass_users'],'PASSWORD_BCRYPT');
 				$mail_users = mysql_real_escape_string($_POST['mail_users']);
 				$nom_users = mysql_real_escape_string($_POST['nom_users']);
 
@@ -1940,7 +1940,7 @@ Lien vers la modération : ".URL.'/admin.php?id='.$arrayObs['id_poi']."\n".
 
 				$lib_users = mysql_real_escape_string($_POST['lib_users']);
 				$nom_users = mysql_real_escape_string($_POST['nom_users']);
-				$pass_users = mysql_real_escape_string($_POST['pass_users']);
+				$pass_users = create_password_hash($_POST['pass_users'],'PASSWORD_BCRYPT');
 				$mail_users = mysql_real_escape_string($_POST['mail_users']);
 
 				$usertype_id_usertype = $_POST['usertype_id_usertype'];
@@ -1959,7 +1959,7 @@ Vous disposez maintenant d'un compte sur velobs vous permettant de mettre à jou
 ".URL."/admin.php
 Vos identifiants sont :
 	- Login : $lib_users
-	- Mot de passe : $pass_users
+	- Mot de passe : ".$_POST['pass_users']."
 En cas de question, vous pouvez trouver des informations sur https://github.com/2p2r/velobs_web. N'hésitez pas à envoyer un courriel à ". MAIL_ALIAS_OBSERVATION_ADHERENTS . " pour toute question sur VelObs.";
 				sendMail($mail_users, "Création compte sur VelObs", $message);
 				mysql_free_result($result);
@@ -2871,5 +2871,42 @@ Cordialement, l'Association ".VELOBS_ASSOCIATION." :)";
 				// TODO
 				break;
 		}
+	}
+	
+	// When you need to hash a password, just feed it to the function
+	// and it will return the hash which you can store in your database.
+	// The important thing here is that you don’t have to provide a salt
+	// value or a cost parameter. The new API will take care of all of
+	// that for you. And the salt is part of the hash, so you don’t
+	// have to store it separately.
+	//
+	// Links:
+	// http://www.sitepoint.com/hashing-passwords-php-5-5-password-hashing-api/
+	// http://stackoverflow.com/questions/536584/non-random-salt-for-password-hashes/536756#536756
+	//
+	// Here is a imlementation for PHP 5.5 and older:
+	function create_password_hash($strPassword, $numAlgo = 1, $arrOptions = array())
+	{
+		if (function_exists('password_hash')) {
+			// php >= 5.5
+			$hash = password_hash($strPassword, $numAlgo, $arrOptions);
+		} else {
+			$salt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
+			$salt = base64_encode($salt);
+			$salt = str_replace('+', '.', $salt);
+			$hash = crypt($strPassword, '$2y$10$' . $salt . '$');
+		}
+		return $hash;
+	}
+	function verify_password_hash($strPassword, $strHash)
+	{
+		if (function_exists('password_verify')) {
+			// php >= 5.5
+			$boolReturn = password_verify($strPassword, $strHash);
+		} else {
+			$strHash2 = crypt($strPassword, $strHash);
+			$boolReturn = $strHash == $strHash2;
+		}
+		return $boolReturn;
 	}
 ?>
