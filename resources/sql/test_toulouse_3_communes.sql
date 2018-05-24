7fa72f9f559ec5376d6b2fd55a93d613f47e9072
gerald2545 <gerald.salin@gmail.com>
2018-05-24 21:01:16 +0200

    #178

---
+++ resources/sql/test_toulouse_3_communes.sql
@@ -0,0 +1,14 @@
+--
+-- import test_communes_agglo_toulouse.csv > TABLE xx
+-- import test_communes_agglo_toulouse.shp > TABLE yy
+--
+
+--TODO : rename TABLE xx and TABLE yy to their real names given by the import procedure (e.g. from within phpmyadmin)
+RENAME TABLE `TABLE xx` TO mes_communes_csv;
+RENAME TABLE `TABLE yy` TO mes_communes_shp;
+--add an index, named id, to each newly created table which will allow to join spatial data with attributes
+ALTER TABLE mes_communes_csv ADD `id` SERIAL NOT NULL;
+ALTER TABLE mes_communes_shp ADD `id` SERIAL NOT NULL;
+
+INSERT INTO `commune`(`id_commune`, `lib_commune`, `geom_commune`) SELECT mes_communes_csv.insee, mes_communes_csv.nom, mes_communes_shp .`SPATIAL` FROM mes_communes_shp  INNER JOIN mes_communes_csv WHERE mes_communes_csv.id =mes_communes_shp.id;
+
---
+++ resources/sql/test_toulouse_configmap.sql
@@ -0,0 +1,6 @@
+--
+-- Contenu de la table `configmap`
+--
+
+INSERT INTO `configmap` (`id_configmap`, `lat_configmap`, `lon_configmap`, `zoom_configmap`, `baselayer_configmap`) VALUES
+(1, 43.6647, 1.48076, 19, 0);
