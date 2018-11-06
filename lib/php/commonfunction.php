57f902a9a0df7875cfe1ab17d3c4dee5f3855ab3
Adrien Loxq <adrien.loxq@gmail.com>
2018-11-06 20:23:05 +0100

    correction lien (encore) logo mail

--- lib/php/commonfunction.php
+++ lib/php/commonfunction.php
@@ -749,7 +749,7 @@
 			error_log(date("Y-m-d H:i:s") . " " .__FUNCTION__ . " - commonfunction.php - Mail avec comme sujet = ".MAIL_SUBJECT_PREFIX . ' '.$subject ." et envoyé à " . $to ."\n", 3, LOG_FILE);
 		}
 
-		$logodata = file_get_contents('../../resources/images/logo-main.png');
+		$logodata = file_get_contents('resources/images/logo-main.png');
 		$b64logo = base64_encode($logodata);
 	
 		$message = '--boundary-string' . $NL . $NL .
