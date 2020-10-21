bc01085c16b26fb63823d36685a152ad11761e75
gerald2545 <gerald.salin@protonmail.com>
2020-10-21 16:10:10 +0200

    #251 ajout du bouton sur l'interface publique + changement de nom du script

--- lib/php/public/getJsonObsVotes.php
+++ lib/php/public/getJsonObsVotes.php
@@ -14,6 +14,10 @@ include_once '../key.php';
 			if (isset($_GET['nb2return']) && is_numeric($_GET['nb2return']) && $_GET['nb2return'] > 0){
 				$nbObservationsToReturn = $_GET['nb2return'];
 			}
+			$groupByCity = 0;
+			if (isset($_GET['groupByCity']) && is_numeric($_GET['groupByCity']) && $_GET['groupByCity'] == 1){
+				$groupByCity = 1;
+			}
 			$sql = "SELECT count(*) as nb_votes, poi_poi_id, lib_subcategory, lib_pole, lib_territoire, lib_commune 
 			FROM `support_poi` 
 			INNER JOIN poi ON poi.id_poi = support_poi.poi_poi_id 
@@ -26,10 +30,36 @@ include_once '../key.php';
 
 			$result = mysql_query($sql);
 			$i = 0;
+			$arr = array();
+			if ($groupByCity){
+			while ($row = mysql_fetch_array($result)){
+				if (count(array_keys($arr[$row['lib_commune']])) >  $nbObservationsToReturn-1){
+					continue;
+				}
+					$arr[$row['lib_commune']][$i]['nbVotes'] = $row['nb_votes'];
+					$arr[$row['lib_commune']][$i]['id_poi'] = $row['poi_poi_id'];
+					$arr[$row['lib_commune']][$i]['url'] = URL."/index.php?id=".$row['poi_poi_id'];
+					$arr[$row['lib_commune']][$i]['categorie'] = $row['lib_subcategory'];
+					$arr[$row['lib_commune']][$i]['territoire'] = $row['lib_territoire'] . " - " . $row['lib_pole'];
+					$i++;
+			}
+			$topVotedText = "";
+			$cities = array_keys($arr);
+			sort($cities);
+			foreach($cities as $city){
+			$topVotedText .= "<div class=\"voteTerritoire\"><H1>Commune : " . $city ."</H1>\r\n";
+						$topVotedText .=  "<ul>";
+						foreach(array_keys($arr[$city]) as $mostVoted){
+							$topVotedText .=  "<li>".$arr[$city][$mostVoted]['nbVotes']." vote(s) pour l'observation <a href=\"".$arr[$city][$mostVoted]['url']."\" target=\"_blank\">".$arr[$city][$mostVoted]['id_poi']."</a> (catégorie ".$arr[$city][$mostVoted]['categorie'].", localisée sur ".$arr[$city][$mostVoted]['territoire'].")</li>\r\n";
+						
+						}
+						$topVotedText .=  "</ul></div>\r\n";
+				}
+			}else{
 			
 			while ($row = mysql_fetch_array($result)){
-				if (count($arr[$row['lib_territoire']][$row['lib_pole']][$row['lib_commune']]) >  $nbObservationsToReturn){
-					next;
+				if (count(array_keys($arr[$row['lib_territoire']][$row['lib_pole']][$row['lib_commune']])) >  $nbObservationsToReturn-1){
+					continue;
 				}
 					$arr[$row['lib_territoire']][$row['lib_pole']][$row['lib_commune']][$i]['nbVotes'] = $row['nb_votes'];
 					$arr[$row['lib_territoire']][$row['lib_pole']][$row['lib_commune']][$i]['id_poi'] = $row['poi_poi_id'];
@@ -46,7 +76,7 @@ include_once '../key.php';
 						$topVotedText .=  "<div class=\"voteCommune\"><H3>Commune : " . $commune ."</H3>\r\n";
 						$topVotedText .=  "<ul>";
 						foreach(array_keys($arr[$territoire][$pole][$commune]) as $mostVoted){
-							$topVotedText .=  "<li>Nombre de votes : ".$arr[$territoire][$pole][$commune][$mostVoted]['nbVotes']." votes pour l'observation <a href=\"".$arr[$territoire][$pole][$commune][$mostVoted]['url']."\" targe=\"_blank\">".$arr[$territoire][$pole][$commune][$mostVoted]['id_poi']."</a> (catégorie ".$arr[$territoire][$pole][$commune][$mostVoted]['categorie'].")</li>\r\n";
+							$topVotedText .=  "<li>".$arr[$territoire][$pole][$commune][$mostVoted]['nbVotes']." vote(s) pour l'observation <a href=\"".$arr[$territoire][$pole][$commune][$mostVoted]['url']."\" target=\"_blank\">".$arr[$territoire][$pole][$commune][$mostVoted]['id_poi']."</a> (catégorie ".$arr[$territoire][$pole][$commune][$mostVoted]['categorie'].")</li>\r\n";
 						
 						}
 						$topVotedText .=  "</ul></div>\r\n";
@@ -54,11 +84,19 @@ include_once '../key.php';
 					}
 				}
 			}
-			
+			}
 			//echo '{"statistiques":'.json_encode($arr).'}';
 			$html = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
-   \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html><head><title>Observations ouvertes les plus votées, par territoire et commune</title></head><body>\r\n";
+   \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html><head><title>Observations ouvertes les plus votées, par territoire et commune</title></head><body class=\"votes\">\r\n";
 			$html.= "<link rel=\"stylesheet\" type=\"text/css\" href=\"../../../resources/css/public.css?v1.7.1\" />\r\n";
+			$html.= "<H1>Listing des observations les plus votées (1 vote est associé à une adresse email)";
+			if($groupByCity){
+			    $html.=  " classées par commune.</H1>";
+			    $html.= "Accéder aux observations les plus plebiscitées par <a href=\"./getJsonObsVotes.php?groupByCity=0\">territoire</a>";
+			}else{
+			    $html.=  " classées par territoire, puis commune.</H1>";
+			    $html.= "Accéder aux observations les plus plebiscitées par <a href=\"./getJsonObsVotes.php?groupByCity=1\">commune</a>";
+			}
 			echo $html;
 			echo $topVotedText;
 			echo "</body></html>";
